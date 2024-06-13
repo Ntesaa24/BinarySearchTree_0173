63a06bf02c06b9be1e654377ea801a997359bbd5
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-06-13 10:55:52 +0700

    allcate memory, assign,make

--- Tree.cpp
+++ Tree.cpp
@@ -30,6 +30,9 @@ public:
 
 	void insert(string element) // Insert a node in the binary seacrh tree 
 	{
-
+		Node* newNode = new Node(element, nullptr, nullptr);  //Allcate memory for the new node 
+		newNode->info = element; //Assign value to the data field of the new node
+		newNode->leftchild = nullptr; //Make the right child of the new node point to NULL 
+		newNode->rightchild = nullptr; // Make the right child of the new node point to NULL 
 	}
 };
